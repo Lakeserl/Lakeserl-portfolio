5096f82113c258af460690612d4dff3e5fcd9cda
Lakeserl <dinhlam18112003@gmail.com>
2025-09-11 00:50:43 +0700

    Updated:project

--- src/app/component/home/more-project/more-project.ts
+++ src/app/component/home/more-project/more-project.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from '../../../service/analytic/analytics-service';
+import { NavigationEnd, Router } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-more-project',
-  imports: [],
+  imports: [TranslateModule, CommonModule, FormsModule, ReactiveFormsModule, ],
   templateUrl: './more-project.html',
   styleUrl: './more-project.scss'
 })
-export class MoreProject {
+export class MoreProject implements OnInit{
 
+  constructor(
+    private router: Router,
+    public analyticsService: AnalyticsService
+  ){}
+
+
+
+  ngOnInit(): void {
+    this.router.events.subscribe((evt) => {
+      if( !(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0)
+    });
+  }
+    redirect(route: string, event: any) {
+      const id = event.target.id;
+      if(id=='demoLink' || id=='ghLink'){
+        return
+      }
+      window.open(route, '_blank');
+    }
 }
--- src/app/component/home/project/project.ts
+++ src/app/component/home/project/project.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { AnalyticsService } from '../../../service/analytic/analytics-service';
 
+
 @Component({
   selector: 'app-project',
   imports: [TranslateModule, TranslateModule, CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
@@ -12,41 +13,22 @@ import { AnalyticsService } from '../../../service/analytic/analytics-service';
   styleUrl: './project.scss'
 })
 export class Project implements OnInit{
+ @ViewChild('imgContainer') imgContainer!: ElementRef;
 
-  @ViewChild('imgContainer') imgContainer: ElementRef | undefined;
-  
   constructor(
     public analyticsService: AnalyticsService
-  ){}
+  ) { }
+
+  
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.initializeCarousels();
-    })
   }
 
-  private initializeCarousels():void {
-        const carousels = document.querySelectorAll('[data-bs-ride="carousel"]');
-    carousels.forEach((carousel: any) => {
-      // Bootstrap 5 carousel will auto-initialize
-      carousel.addEventListener('slide.bs.carousel', (event: any) => {
-        // Optional: Handle carousel slide events
-      });
+  debug(){
+    this.imgContainer.nativeElement.scroll({
+      top: this.imgContainer.nativeElement.scrollHeight,
+      left: 0,
+      behavior: 'smooth',    
     });
   }
-
-  debug(): void {
-    if (this.imgContainer) {
-      this.imgContainer.nativeElement.scroll({
-        top: this.imgContainer.nativeElement.scrollHeight,
-        left: 0,
-        behavior: 'smooth',
-      });
-    }
-  }
-
-  getCarouselId(index: number): string {
-    return `projectCarousel${index}`;
-  }
-
 }
